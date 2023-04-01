359d7c3f3504e86d8c52f1c86cf3642c75a2318b
Ebrar <16155052@mersin.edu.tr>
2023-04-01 17:21:09 +0300

    string manipulator

---
+++ string-manipulator/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+const usage = `[command] [string]
+Available commands: lower, upper and title
+`
+
+func main() {
+	args := os.Args
+	if len(args) != 3 {
+		fmt.Println(usage)
+		return
+	}
+
+	cmd, str := args[1], args[2]
+
+	switch cmd {
+	case "lower":
+		fmt.Println(strings.ToLower(str))
+	case "upper":
+		fmt.Println(strings.ToUpper(str))
+	case "title":
+		fmt.Println(strings.Title(str))
+	default:
+		fmt.Println("unknown command %q\n", cmd)
+	}
+
+}
