d0dafae1ac8481d96021deb4db1292cc387e025c
Ebrar <16155052@mersin.edu.tr>
2023-04-02 13:29:50 +0300

    random  numberr

--- random-number/main.go
+++ random-number/main.go
@@ -3,19 +3,51 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
 
+const (
+	maxTurns = 5 // less is more difficult
+	usage    = `Welcome to the Lucky Number Game! 🍀
+The program will pick %d random numbers.
+Your mission is to guess one of those numbers.
+The greater your number is, harder it gets.
+Wanna play?
+`
+)
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
 
-	t := time.Now()
-	rand.Seed(t.UnixNano())
-	rand.Seed(100)
-	guess := 13
+	args := os.Args[1:]
 
-	for n := 0; n != guess; {
-		n = rand.Intn(guess + 1)
-		fmt.Printf("%d\n", n)
+	if len(args) != 1 {
+		// fmt.Println("Pick a number.")
+		fmt.Printf(usage, maxTurns)
+		return
 	}
-	fmt.Println()
+
+	guess, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Not a number.")
+		return
+	}
+
+	if guess < 0 {
+		fmt.Println("Please pick a positive number.")
+		return
+	}
+
+	for turn := 0; turn < maxTurns; turn++ {
+		n := rand.Intn(guess + 1)
+
+		if n == guess {
+			fmt.Println("🎉  YOU WIN!")
+			return
+		}
+	}
+
+	fmt.Println("☠️  YOU LOST... Try again?")
 }
